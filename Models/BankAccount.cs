9fac1e8bce6666912ebe5a038c78a35026f9cec4
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-19 19:27:32 +0530

    innit

---
+++ Models/BankAccount.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManagementSystem.Models
+{
+    internal class BankAccount
+    {
+        static int MINIMUM_BALANCE = 500;
+
+        private int _accountNumber;
+        private string _accountName;
+        private int _balance;
+
+
+        public int AccountNumber
+        {
+            set { _accountNumber = value; }
+            get { return _accountNumber; }
+        }
+        public string Name
+        {
+            set { _accountName = value; }
+            get { return _accountName; }
+        }
+
+        public int Balance
+        {
+            set
+            {
+                if (value < MINIMUM_BALANCE)
+                    _balance = MINIMUM_BALANCE;
+                else
+                    _balance = value;
+            }
+            get { return _balance; }
+        }
+
+        public BankAccount(int accountNumber, string name)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = MINIMUM_BALANCE;
+        }
+
+        public BankAccount(int accountNumber, string name, int balance)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = balance;
+        }
+
+
+        public override string ToString()
+        {
+            Console.WriteLine($"=========Account No {AccountNumber}===========");
+            Console.WriteLine($"Bank Account no: {AccountNumber}");
+            Console.WriteLine($"User Name no: {Name}");
+            Console.WriteLine($"Bank Balance : {Balance}");
+            Console.WriteLine("==================================");
+            return " ";
+        }
+
+
+        public bool WithDrawMoney(int money)
+        {
+            if (Balance - money < MINIMUM_BALANCE)
+                return false;
+
+            Balance -= money;
+            return true;
+        }
+
+        public bool DepositMoney(int money)
+        {
+            Balance = Balance + money;
+            return true;
+        }
+
+        public static BankAccount AccountWithMaxBalance(BankAccount[] account)
+        {
+            double maxBalance = 0;
+            int temp = 0;
+            for (int i = 0; i < account.Length; i++)
+            {
+                if (account[i].Balance > maxBalance)
+                    maxBalance = account[i].Balance;
+                temp = i;
+            }
+
+            return account[temp];
+
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using MovieStoreApp.Models;
+
+namespace MovieStoreApp
+{
+    internal class Program
+    {
+        static List<Movie> movies = new List<Movie>(4);
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+        }
+
+        static void DisplayMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to MovieStore App");
+                Console.WriteLine("What operation would you like to perform? \n" +
+                    "1. Add new Movie \n" +
+                    "2. Display All Movie \n" +
+                    "3. Find Movie by Id \n" +
+                    "4. Remove Movie by Name \n" +
+                    "5. Clear All Movie \n" +
+                    "6. Exit \n");
+
+                Console.WriteLine("Enter your Choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                DoTask(choice);
+            }
+        }
+
+        static void DoTask(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    if(movies.Count >4)
+                        Console.WriteLine("List is full");
+                    else
+                        AddMovie();
+                    break;
+                case 2:
+                    movies.ForEach(movie => Console.WriteLine(movie));
+                    break;
+                case 3:
+                    Movie findMovie = FindMovieById();
+                    if (findMovie != null)
+                        Console.WriteLine(findMovie);
+                    else
+                        Console.WriteLine("Account not found");
+                    break;
+                case 4:
+                    RemoveMovie();
+                    break;
+                case 5:
+                    if(movies.Count ==0)
+                        Console.WriteLine("Movie list is already empty. Nothing to clear");
+                    else
+                        movies.Clear();
+                    break;
+                case 6:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break ;
+
+
+
+            }
+        }
+
+        static void AddMovie()
+        {
+            Console.WriteLine("Enter Movie Details");
+            Console.WriteLine("Enter Movie Id");
+            int movieId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Movie Name");
+            string movieName = Console.ReadLine();
+          //  Console.WriteLine("Enter Year of Release");
+           // int year = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine("Enter Genre");
+           // string genre = Console.ReadLine();
+
+            movies.Add(Movie.CreateNewMovie(movieId, movieName, 2002, "comedy"));
+        }
+
+        static Movie FindMovieById()
+        {
+            Movie findMovie = null;
+            Console.WriteLine("Enter Movie Id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            findMovie = movies.Where(item => item.Id == id).FirstOrDefault();
+            return findMovie;
+
+        }
+        static void RemoveMovie()
+        {
+            Movie findMovie = null;
+            Console.WriteLine("Enter Movie Name");
+            string name = Console.ReadLine();
+            findMovie = movies.Where(item => item.Name == name).FirstOrDefault();
+            if (findMovie == null)
+                Console.WriteLine("Account not found");
+            else
+            {
+                movies.Remove(findMovie);
+                Console.WriteLine("Movie deleted successfully");
+            }
+        }
+    }
+}
